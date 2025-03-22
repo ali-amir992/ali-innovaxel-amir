86c03b442f3be4a073df7961559b08e694705ff9
ali-amir992 <alimuneer992@gmail.com>
2025-03-22 14:05:36 +0500

    setup front with react vite and tailwind

--- server/index.ts
+++ server/index.ts
@@ -4,9 +4,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import urlRoutes from "@routes/URL"
 import { dbConnect } from "@config/dbConnect";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
